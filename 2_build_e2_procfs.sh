cfb7e5a84c20e986d50c7fe52f95e6c7c5442e15
ag1455 <a.g.prosat@gmail.com>
2022-02-04 22:51:34 +0100

    Fix build e2_procfs.

--- 2_build_e2_procfs.sh
+++ 2_build_e2_procfs.sh
@@ -3,8 +3,9 @@
 M="e2_procfs"
 
 cp -fv pre/$M.conf /lib/modules-load.d
-cd $M
+cd pre/$M
 make -C /lib/modules/`uname -r`/build M=`pwd`
-cp -fv $M.ko /lib/modules/`uname -r`/kernel/drivers/media/dvb-frontends
-depmod -a
+cp -fv $M.ko /lib/modules/`uname -r`/kernel/drivers/media/common
 make clean
+cd ../..
+depmod -a
