317398d4ce45e8f639c63c605ef146be4e4a2b06
ag1455 <a.g.prosat@gmail.com>
2022-05-26 13:22:07 +0300

    Minor fixes.

--- 6_build_plugins.sh
+++ 6_build_plugins.sh
@@ -376,9 +376,9 @@ if [ -d plugins ]; then
 		unzip master.zip
 		rm master.zip
 		mv $PKG-master $PKG
-		rpl "Foreca - прогноз погоды" "'Foreca' - Прогноз погоды" $PKG/plugin/locale/ru/LC_MESSAGES/Foreca.po
-		rpl '\x1b' 'KEY_ESC' $PKG/plugin/keymap.xml
 		cd $PKG
+		rpl "Foreca - прогноз погоды" "'Foreca' - Прогноз погоды" plugin/locale/ru/LC_MESSAGES/Foreca.po
+		rpl -F "\x1b" "KEY_ESC" plugin/keymap.xml
 		if [ "$release" = "20.04" ]; then
 			python2 setup.py install
 		elif [ "$release" = "22.04" ]; then
@@ -517,7 +517,7 @@ if [ -d plugins ]; then
 		PKG="e2iplayer"
 		PKG_="IPTVPlayer"
 		PKG__="E2IPlayer"
-		VER="8c58a4817dfc2d0fdf3305f143d755aefdad3d70"
+		VER="c81ec91a33b8122b1dc893fb195ffd02fca0ebc3"
 		if [ -d $PKG ]; then
 			rm -rf $PKG
 		fi
